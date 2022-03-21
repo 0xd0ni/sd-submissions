401eca81707aa7f2429a6354af03b5bc0f33b6b1
CarolinaBaptista98 <acarolbaptista1@gmail.com>
2022-03-21 22:39:22 +0000

    clientes

--- Professor/src/main/java/pt/ulisboa/tecnico/classes/professor/Professor.java
+++ Professor/src/main/java/pt/ulisboa/tecnico/classes/professor/Professor.java
@@ -1,12 +1,59 @@
 package pt.ulisboa.tecnico.classes.professor;
 
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import pt.ulisboa.tecnico.classes.contract.ClassesDefinitions;
+import pt.ulisboa.tecnico.classes.contract.professor.ProfessorClassServer;
+import pt.ulisboa.tecnico.classes.contract.professor.ProfessorServiceGrpc;
+
+import java.util.Scanner;
+
 public class Professor {
 
   public static void main(String[] args) {
-    System.out.println(Professor.class.getSimpleName());
-    System.out.printf("Received %d Argument(s)%n", args.length);
-    for (int i = 0; i < args.length; i++) {
-      System.out.printf("args[%d] = %s%n", i, args[i]);
+    Scanner input = new Scanner(System.in);
+
+    final String serverAddress = "localhost:5000";
+
+    final ManagedChannel channel = ManagedChannelBuilder.forTarget(serverAddress).usePlaintext().build();
+    ProfessorServiceGrpc.ProfessorServiceBlockingStub stub = ProfessorServiceGrpc.newBlockingStub(channel);
+
+
+    while(input.hasNextLine()){
+      System.out.print("> ");
+      System.out.flush();
+      String[] line = input.nextLine().split(" ");
+      String command = line[0];
+
+      System.out.println(command);
+
+      if (command.equals("list")){
+        ProfessorClassServer.ListClassRequest listRequest = ProfessorClassServer.ListClassRequest.newBuilder().build();
+        ProfessorClassServer.ListClassResponse listResponse = stub.listClass(listRequest);
+
+        //System.out.println(listResponse.getCode());
+        //System.out.println(Stringfy.format(listResponse.getClassState()));
+
+        System.out.println(listResponse);
+      }
+
+      else if (command.equals("openEnrollments")) {
+        Integer numStudents = Integer.parseInt(line[1]);
+
+        ProfessorClassServer.OpenEnrollmentsRequest openEnrollmentsRequest = ProfessorClassServer.OpenEnrollmentsRequest.newBuilder().setCapacity(numStudents).build();
+        ProfessorClassServer.OpenEnrollmentsResponse openEnrollmentsResponse = stub.openEnrollments(openEnrollmentsRequest);
+
+        System.out.println(openEnrollmentsResponse);
+
+      }
+      else if (command.equals("exit")) {
+        channel.shutdownNow();
+        System.exit(0);
+      }
+      else {
+        System.out.println("Invalid command");
+      }
     }
   }
 }
+
--- Student/src/main/java/pt/ulisboa/tecnico/classes/student/Student.java
+++ Student/src/main/java/pt/ulisboa/tecnico/classes/student/Student.java
@@ -7,40 +7,68 @@ import pt.ulisboa.tecnico.classes.contract.ClassesDefinitions;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 
+import java.util.Scanner;
+
 public class Student {
 
+  static private String name;
+  static private String id;
+
   public static void main(String[] args) throws Exception {
-    System.out.println(Student.class.getSimpleName());
-    System.out.printf("Received %d Argument(s)%n", args.length);
-    for (int i = 0; i < args.length; i++) {
-      System.out.printf("args[%d] = %s%n", i, args[i]);}
+
+    //name = "João";
+    //id = "Aluno001";
 
     if (args.length < 2) {
       System.err.println("Argument(s) missing!");
-      System.err.printf("Usage: java %s host port%n", Student.class.getName());
+      System.err.printf("Usage: java %s <id> <name>\n", Student.class.getName());
       return;
     }
 
-    final String host = args[0];
-    final int port = Integer.parseInt(args[1]);
-    final String target = host + ":" + port;
+    id = args[0];
+    name = "";
+    for (int j = 1; j < args.length; j++) {
+      name.concat(args[j]);
+      if (j == args.length - 1)
+        name.concat(" ");
+    }
+    System.out.printf("Student's id %s, and name %s\n", id, name);
 
-    final ManagedChannel channel = ManagedChannelBuilder.forTarget(target).usePlaintext().build();
+    Scanner input = new Scanner(System.in);
 
+    final String serverAddress = "localhost:5000";
+
+    final ManagedChannel channel = ManagedChannelBuilder.forTarget(serverAddress).usePlaintext().build();
     StudentServiceGrpc.StudentServiceBlockingStub stub = StudentServiceGrpc.newBlockingStub(channel);
-    StudentClassServer.ListClassRequest listRequest = StudentClassServer.ListClassRequest.newBuilder().build();
-    StudentClassServer.ListClassResponse listResponse = stub.listClass(listRequest);
 
-    System.out.println("exibindo alunos:");
-    System.out.println(listResponse);
 
-    ClassesDefinitions.Student student = ClassesDefinitions.Student.newBuilder().setStudentId("22323").setStudentName("Jose").build();
-    StudentClassServer.EnrollRequest enrollRequest = StudentClassServer.EnrollRequest.newBuilder().setStudent(student).build();
-    StudentClassServer.EnrollResponse enrollResponse = stub.enroll(enrollRequest);
+    while(input.hasNextLine()){
+      System.out.print("> ");
+      System.out.flush();
+      String command = input.nextLine();
+      System.out.println(command);
 
-    System.out.println("inscrevendo o aluno:");
-    System.out.println(enrollResponse);
+      if (command.equals("list")){
+        StudentClassServer.ListClassRequest listRequest = StudentClassServer.ListClassRequest.newBuilder().build();
+        StudentClassServer.ListClassResponse listResponse = stub.listClass(listRequest);
 
-    channel.shutdownNow();
+        System.out.println(listResponse);
+      }
+
+      else if (command.equals("enroll")) {
+        ClassesDefinitions.Student student = ClassesDefinitions.Student.newBuilder().setStudentId(id).setStudentName(name).build();
+        StudentClassServer.EnrollRequest enrollRequest = StudentClassServer.EnrollRequest.newBuilder().setStudent(student).build();
+        StudentClassServer.EnrollResponse enrollResponse = stub.enroll(enrollRequest);
+
+        System.out.println(enrollResponse);
+      }
+      else if (command.equals("exit")) {
+        channel.shutdownNow();
+        System.exit(0);
+      }
+      else {
+        System.out.println("Invalid command");
+      }
+    }
   }
 }
