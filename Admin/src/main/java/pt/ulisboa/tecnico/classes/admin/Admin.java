7d6b2941355c0f8c75770a092f68223c22225944
Martimc123 <martim.correia123@hotmail.com>
2022-03-23 22:36:25 +0000

    Cleared and simplefied Admin overall code

--- Admin/src/main/java/pt/ulisboa/tecnico/classes/admin/Admin.java
+++ Admin/src/main/java/pt/ulisboa/tecnico/classes/admin/Admin.java
@@ -1,43 +1,69 @@
 package pt.ulisboa.tecnico.classes.admin;
-import pt.ulisboa.tecnico.classes.contract.admin.AdminClassServer;
+
+import pt.ulisboa.tecnico.classes.contract.admin.AdminClassServer.ActivateResponse;
+import pt.ulisboa.tecnico.classes.contract.admin.AdminClassServer.ActivateRequest;
+import pt.ulisboa.tecnico.classes.contract.admin.AdminClassServer.DeactivateResponse;
+import pt.ulisboa.tecnico.classes.contract.admin.AdminClassServer.DeactivateRequest;
+import pt.ulisboa.tecnico.classes.contract.admin.AdminClassServer.DumpResponse;
+import pt.ulisboa.tecnico.classes.contract.admin.AdminClassServer.DumpRequest;
+import pt.ulisboa.tecnico.classes.Stringify;
+import pt.ulisboa.tecnico.classes.contract.ClassesDefinitions.ResponseCode;
 
 import java.util.Scanner;
 
 public class Admin {
 
+  private static final String EXIT_CMD = "exit";
+  private static final String ACTIV_CMD = "activate";
+  private static final String DEACT_CMD = "deactivate";
+  private static final String DUMP_CMD = "dump";
+
+
   public static void main(String[] args) {
-    Scanner input = new Scanner(System.in);
+    System.out.println(Admin.class.getSimpleName());
 
-    AdminFrontend frontend = new AdminFrontend("localhost", 5000);
+    final String host = "localhost";
+    final int port = 5000;
 
-    while(input.hasNextLine()){
-      System.out.print("> ");
-      System.out.flush();
-      String[] line = input.nextLine().split(" ");
-      String command = line[0];
+    try (AdminFrontend frontend = new AdminFrontend(host, port); Scanner scanner = new Scanner(System.in)) {
+      while (true) {
+        System.out.printf("%n> ");
+        try {
+          String line = scanner.nextLine();
+          switch (line)
+          {
+            case EXIT_CMD:
+              break;
 
-      System.out.println(command);
+            case DUMP_CMD:
+              DumpRequest dump_req = DumpRequest.newBuilder().build();
+              DumpResponse dump_res = frontend.setDump(dump_req);
+              if (ResponseCode.forNumber(frontend.getCodeDump(dump_res)) == ResponseCode.OK)
+                System.out.println(Stringify.format(frontend.getClassState(dump_res)));
+              break;
 
-      if (command.equals("dump")){
-        AdminClassServer.DumpResponse dumpResponse = frontend.dump();
-        System.out.println(dumpResponse);
+            case ACTIV_CMD:
+              ActivateRequest req = ActivateRequest.newBuilder().build();
+              ActivateResponse res = frontend.setActivate(req);
+              if (ResponseCode.forNumber(frontend.getCode(res)) == ResponseCode.OK)
+                System.out.println("The action completed successfully.");
+              break;
+
+            case DEACT_CMD:
+              DeactivateRequest d_req = DeactivateRequest.newBuilder().build();
+              DeactivateResponse d_res = frontend.setDeactivate(d_req);
+              if (ResponseCode.forNumber(frontend.getCodeD(d_res)) == ResponseCode.OK)
+                System.out.println("The action completed successfully.");
+              break;
+          }
+        } catch (NullPointerException e) {
+          System.out.println("NULL");
+        }
       }
 
-      else if (command.equals("activate")){
-        AdminClassServer.ActivateResponse activateResponse = frontend.activate();
-        System.out.println(activateResponse);
-      }
-      else if (command.equals("deactivate")){
-        AdminClassServer.DeactivateResponse deactivateResponse = frontend.deactivate();
-        System.out.println(deactivateResponse);
-      }
-      else if (command.equals("exit")) {
-        frontend.close();
-        System.exit(0);
-      }
-      else {
-        System.out.println("Invalid command");
-      }
+    } finally {
+      System.out.println("");
     }
   }
+
 }
--- Admin/src/main/java/pt/ulisboa/tecnico/classes/admin/AdminFrontend.java
+++ Admin/src/main/java/pt/ulisboa/tecnico/classes/admin/AdminFrontend.java
@@ -1,7 +1,14 @@
 package pt.ulisboa.tecnico.classes.admin;
-import pt.ulisboa.tecnico.classes.contract.admin.AdminClassServer;
+
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
+import pt.ulisboa.tecnico.classes.contract.admin.AdminClassServer.ActivateResponse;
+import pt.ulisboa.tecnico.classes.contract.admin.AdminClassServer.ActivateRequest;
+import pt.ulisboa.tecnico.classes.contract.admin.AdminClassServer.DeactivateRequest;
+import pt.ulisboa.tecnico.classes.contract.admin.AdminClassServer.DeactivateResponse;
+import pt.ulisboa.tecnico.classes.contract.admin.AdminClassServer.DumpResponse;
+import pt.ulisboa.tecnico.classes.contract.admin.AdminClassServer.DumpRequest;
+import pt.ulisboa.tecnico.classes.contract.ClassesDefinitions.ClassState;
 import pt.ulisboa.tecnico.classes.contract.admin.AdminServiceGrpc;
 
 public class AdminFrontend implements AutoCloseable {
@@ -9,26 +16,43 @@ public class AdminFrontend implements AutoCloseable {
     private final AdminServiceGrpc.AdminServiceBlockingStub stub;
 
     public AdminFrontend(String host, int port) {
+        // Channel is the abstraction to connect to a service endpoint.
+        // Let us use plaintext communication because we do not have certificates.
         this.channel = ManagedChannelBuilder.forAddress(host, port).usePlaintext().build();
-        this.stub = AdminServiceGrpc.newBlockingStub(channel);
+
+        // Create a blocking stub.
+        stub = AdminServiceGrpc.newBlockingStub(channel);
     }
 
-    public AdminClassServer.DumpResponse dump() {
-        AdminClassServer.DumpRequest dumpRequest = AdminClassServer.DumpRequest.newBuilder().build();
-        AdminClassServer.DumpResponse dumpResponse = stub.dump(dumpRequest);
-        return dumpResponse;
+    public int getCode(ActivateResponse response) {
+        return response.getCodeValue();
     }
 
-    public AdminClassServer.ActivateResponse activate() {
-        AdminClassServer.ActivateRequest activateRequest = AdminClassServer.ActivateRequest.newBuilder().build();
-        AdminClassServer.ActivateResponse activateResponse = stub.activate(activateRequest);
-        return activateResponse;
+    public ClassState getClassState(DumpResponse response) {
+        return response.getClassState();
     }
 
-    public AdminClassServer.DeactivateResponse deactivate() {
-        AdminClassServer.DeactivateRequest deactivateRequest = AdminClassServer.DeactivateRequest.newBuilder().build();
-        AdminClassServer.DeactivateResponse deactivateResponse = stub.deactivate(deactivateRequest);
-        return deactivateResponse;
+    public int getCodeD(DeactivateResponse response) {
+        return response.getCodeValue();
+    }
+
+    public int getCodeDump(DumpResponse response) {
+        return response.getCodeValue();
+    }
+
+    public ActivateResponse setActivate(ActivateRequest request)
+    {
+        return stub.activate(request);
+    }
+
+    public DeactivateResponse setDeactivate(DeactivateRequest request)
+    {
+        return stub.deactivate(request);
+    }
+
+    public DumpResponse setDump(DumpRequest request)
+    {
+        return stub.dump(request);
     }
 
     @Override
