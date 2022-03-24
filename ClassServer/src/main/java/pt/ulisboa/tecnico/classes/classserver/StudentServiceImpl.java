9443c882de2afd147675b772c45d86a880f6b34b
alravid <allancravid@tecnico.ulisboa.pt>
2022-03-24 17:02:06 +0000

    Add enroll service impl. > Co-authored-by: Martim Correia <martim.correia123@hotmail.com>

--- ClassServer/src/main/java/pt/ulisboa/tecnico/classes/classserver/StudentServiceImpl.java
+++ ClassServer/src/main/java/pt/ulisboa/tecnico/classes/classserver/StudentServiceImpl.java
@@ -2,11 +2,17 @@ package pt.ulisboa.tecnico.classes.classserver;
 
 
 import io.grpc.stub.StreamObserver;
+import static io.grpc.Status.INVALID_ARGUMENT;
 
 import pt.ulisboa.tecnico.classes.classserver.domain.ClassState;
+import pt.ulisboa.tecnico.classes.classserver.exception.ClassesException;
+import pt.ulisboa.tecnico.classes.contract.ClassesDefinitions;
 import pt.ulisboa.tecnico.classes.contract.student.StudentServiceGrpc;
 import pt.ulisboa.tecnico.classes.contract.student.StudentClassServer;
 import pt.ulisboa.tecnico.classes.classserver.domain.ClassState;
+import pt.ulisboa.tecnico.classes.classserver.domain.Student;
+
+
 
 
 public class StudentServiceImpl extends StudentServiceGrpc.StudentServiceImplBase {
@@ -19,7 +25,7 @@ public class StudentServiceImpl extends StudentServiceGrpc.StudentServiceImplBas
 
     @Override
     public void listClass(StudentClassServer.ListClassRequest listClassRequest,
-                          StreamObserver<StudentClassServer.ListClassResponse> listClassResponse) {
+                          StreamObserver<StudentClassServer.ListClassResponse> responseObserver) {
 
         // TODO !!
 
@@ -27,9 +33,29 @@ public class StudentServiceImpl extends StudentServiceGrpc.StudentServiceImplBas
 
     @Override
     public  void enroll(StudentClassServer.EnrollRequest enrollRequest,
-                        StreamObserver<StudentClassServer.EnrollResponse> enrollResponse) {
+                        StreamObserver<StudentClassServer.EnrollResponse> responseObserver) {
 
-        // TODO !!
+        ClassesDefinitions.Student toEnroll = enrollRequest.getStudent();
+        String studentName = toEnroll.getStudentName();
+        String studentId = toEnroll.getStudentId();
+
+        
+        try { 
+            Student student = new Student(studentId,studentName);
+
+            // verifies if the student is already enrolled
+            _class.containsStudent(student);
+
+            _class.addEnroll(student);
+
+            responseObserver.onNext(StudentClassServer.EnrollResponse.newBuilder().setCode(
+                    ClassesDefinitions.ResponseCode.OK).build());
+            responseObserver.onCompleted();
+
+        } catch (ClassesException e) {
+            responseObserver.onError(INVALID_ARGUMENT.withDescription(e.getMessage()).asRuntimeException());
+        }
+
     }
 
 }
