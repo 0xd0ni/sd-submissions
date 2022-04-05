46e8dc3abdcede215f17c7ab14c54403f7e7e65c
alravid <allancravid@tecnico.ulisboa.pt>
2022-04-05 16:11:27 +0100

    add debug messages | minor changes

--- ClassServer/src/main/java/pt/ulisboa/tecnico/classes/classserver/StudentServiceImpl.java
+++ ClassServer/src/main/java/pt/ulisboa/tecnico/classes/classserver/StudentServiceImpl.java
@@ -22,13 +22,13 @@ public class StudentServiceImpl extends StudentServiceGrpc.StudentServiceImplBas
         this.DEBUG_VALUE = debugValue;
 
     }
-
-
+    
     @Override
     public void listClass(StudentClassServer.ListClassRequest listClassRequest,
                           StreamObserver<StudentClassServer.ListClassResponse> responseObserver) {
         debug("listClass...");
 
+        debug(" 'listClass' building the response");
         StudentClassServer.ListClassResponse response = StudentClassServer.ListClassResponse.newBuilder().setCode(
                 ClassesDefinitions.ResponseCode.OK).setClassState(
                 ClassesDefinitions.ClassState.newBuilder().setCapacity(_class.getCapacity()).setOpenEnrollments(
@@ -36,7 +36,7 @@ public class StudentServiceImpl extends StudentServiceGrpc.StudentServiceImplBas
                         _class.getEnrolled())).addAllDiscarded(Utils.StudentWrapper(
                         _class.getDiscarded()))).build();
 
-
+        debug(" 'listClass' responding to the request");
         responseObserver.onNext(response);
         responseObserver.onCompleted();
 
@@ -52,9 +52,13 @@ public class StudentServiceImpl extends StudentServiceGrpc.StudentServiceImplBas
         String studentName = toEnroll.getStudentName();
         String studentId = toEnroll.getStudentId();
 
+        debug(" 'enroll' creating a new student");
         Student student = new Student(studentId,studentName);
 
+        debug(" 'enroll' performing validations");
         if(!_class.getOpenEnrollments()) {
+
+            debug(" 'enroll' responding to the request [due to validation]");
             responseObserver.onNext(StudentClassServer.EnrollResponse.newBuilder().setCode(
                     ClassesDefinitions.ResponseCode.ENROLLMENTS_ALREADY_CLOSED).build());
             responseObserver.onCompleted();
@@ -62,6 +66,8 @@ public class StudentServiceImpl extends StudentServiceGrpc.StudentServiceImplBas
 
         }
         else if(Utils.CheckForUserExistence(studentId,_class)) {
+
+            debug(" 'enroll' responding to the request [due to validation]");
             responseObserver.onNext(StudentClassServer.EnrollResponse.newBuilder().setCode(
                     ClassesDefinitions.ResponseCode.STUDENT_ALREADY_ENROLLED).build());
             responseObserver.onCompleted();
@@ -69,15 +75,23 @@ public class StudentServiceImpl extends StudentServiceGrpc.StudentServiceImplBas
 
         }
         else if(_class.checkForFullCapacity()) {
+
+            debug(" 'enroll' responding to the request [due to validation]");
             responseObserver.onNext(StudentClassServer.EnrollResponse.newBuilder().setCode(
                     ClassesDefinitions.ResponseCode.FULL_CLASS).build());
             responseObserver.onCompleted();
         }
         else {
+
+            debug(" 'enroll' enrolling the student");
             _class.addEnroll(student);
+            debug(" 'enroll' adding the student to the system registry");
             _class.addToRegistry(studentId, student);
+            debug(" 'enroll' increasing the number of enrolled students");
             _class.upEnrolled();
 
+
+            debug(" 'enroll' responding to the request");
             responseObserver.onNext(StudentClassServer.EnrollResponse.newBuilder().setCode(
                     ClassesDefinitions.ResponseCode.OK).build());
             responseObserver.onCompleted();
