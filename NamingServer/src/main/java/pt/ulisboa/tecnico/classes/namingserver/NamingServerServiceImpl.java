08fd8c79c884de27f26eaee4db9fbe3d7728f295
alravid <allancravid@tecnico.ulisboa.pt>
2022-04-08 13:18:26 +0100

    add NamingServerService impl Initial template

--- NamingServer/src/main/java/pt/ulisboa/tecnico/classes/namingserver/NamingServerServiceImpl.java
+++ NamingServer/src/main/java/pt/ulisboa/tecnico/classes/namingserver/NamingServerServiceImpl.java
@@ -1,36 +1,49 @@
 package pt.ulisboa.tecnico.classes.namingserver;
 
 import io.grpc.stub.StreamObserver;
+import pt.ulisboa.tecnico.classes.contract.ClassesDefinitions;
 import pt.ulisboa.tecnico.classes.contract.naming.ClassServerNamingServer;
-//import pt.ulisboa.tecnico.classes.contract.naming.NamingServerServiceGrpc;
-
+import pt.ulisboa.tecnico.classes.contract.naming.NamingServerServiceGrpc;
+import pt.ulisboa.tecnico.classes.namingserver.domain.ServiceEntry;
+import pt.ulisboa.tecnico.classes.namingserver.domain.ServiceEntry;
+import pt.ulisboa.tecnico.classes.namingserver.domain.NamingServices;
 import java.util.logging.Logger;
 
-public class NamingServerServiceImpl {
+public class NamingServerServiceImpl extends NamingServerServiceGrpc.NamingServerServiceImplBase {
+
 
     private static final Logger LOGGER = Logger.getLogger(NamingServerServiceImpl.class.getName());
+
     private final boolean DEBUG_VALUE;
 
-
     public NamingServerServiceImpl(boolean debugValue) {
+
         this.DEBUG_VALUE = debugValue;
+    }
+
+
+    public void register(ClassServerNamingServer.RegisterRequest registerRequest,
+                         StreamObserver<ClassServerNamingServer.RegisterResponse>  responseObserver) {
+
+        debug( "register...");
+        // TODO !!
+
 
     }
 
 
-    //@Override
-    //public synchronized void register(ClassServerNamingServer.registerRequest request,
-    //                                 StreamObserver<ClassServerNamingServer.registerResponse> responseObserver) {
+    public void lookup(ClassServerNamingServer.LookupRequest lookupRequest,
+                       StreamObserver<ClassServerNamingServer.LookupResponse> responseObserver) {
 
-    //    debug("register");
+        debug( "lookup...");
+        // TODO !!
+    }
 
-
-    // }
-
-
-
-
-
+    public void delete(ClassServerNamingServer.DeleteRequest deleteRequest,
+                       StreamObserver<ClassServerNamingServer.DeleteResponse> responseObserver) {
+        debug( "delete...");
+        // TODO !!
+    }
 
     public void debug(String msg) {
         if(DEBUG_VALUE) {
