f1b1f2dd7f1dc80ae8be8c68061404d18857b72b
alravid <allancravid@tecnico.ulisboa.pt>
2022-04-08 19:39:18 +0100

    fix methods signature

--- Utilities/src/main/java/pt/ulisboa/tecnico/classes/NamingServerGlobalFrontend.java
+++ Utilities/src/main/java/pt/ulisboa/tecnico/classes/NamingServerGlobalFrontend.java
@@ -2,14 +2,21 @@ package pt.ulisboa.tecnico.classes;
 
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
+import pt.ulisboa.tecnico.classes.contract.naming.NamingServerServiceGrpc;
 import pt.ulisboa.tecnico.classes.contract.naming.ClassServerNamingServer.LookupResponse;
 import pt.ulisboa.tecnico.classes.contract.naming.ClassServerNamingServer.LookupRequest;
-import pt.ulisboa.tecnico.classes.contract.naming.NamingServerServiceGrpc;
+import pt.ulisboa.tecnico.classes.contract.naming.ClassServerNamingServer.RegisterRequest;
+import pt.ulisboa.tecnico.classes.contract.naming.ClassServerNamingServer.RegisterResponse;
+import pt.ulisboa.tecnico.classes.contract.naming.ClassServerNamingServer.DeleteRequest;
+import pt.ulisboa.tecnico.classes.contract.naming.ClassServerNamingServer.DeleteResponse;
+
 
 public abstract class NamingServerGlobalFrontend {
 
     private final ManagedChannel channel;
     private final NamingServerServiceGrpc.NamingServerServiceBlockingStub stub;
+
+
 
     public NamingServerGlobalFrontend(String host, int port) {
         // Channel is the abstraction to connect to a service endpoint.
@@ -20,7 +27,19 @@ public abstract class NamingServerGlobalFrontend {
         this.stub = NamingServerServiceGrpc.newBlockingStub(channel);
     }
 
-    public void register(){}
-    public LookupResponse lookup(LookupRequest request){return stub.lookup(request);}
-    public void delete(){}
+    public abstract RegisterResponse register(RegisterRequest request);
+
+    public LookupResponse lookup(LookupRequest request) {
+        return stub.lookup(request);
+
+    }
+    public abstract DeleteResponse delete(DeleteRequest request);
+
+    public ManagedChannel getChannel() {
+        return channel;
+    }
+
+    public NamingServerServiceGrpc.NamingServerServiceBlockingStub getStub() {
+        return stub;
+    }
 }
