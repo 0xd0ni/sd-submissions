8754f27440902eacee0ed1944300cd540c59ce0a
alravid <allancravid@tecnico.ulisboa.pt>
2022-04-08 13:19:36 +0100

    add NamingServices to domain

--- NamingServer/src/main/java/pt/ulisboa/tecnico/classes/namingserver/domain/NamingServices.java
+++ NamingServer/src/main/java/pt/ulisboa/tecnico/classes/namingserver/domain/NamingServices.java
@@ -1,11 +1,9 @@
-package pt.ulisboa.tecnico.classes.namingserver;
-
+package pt.ulisboa.tecnico.classes.namingserver.domain;
 
 import pt.ulisboa.tecnico.classes.namingserver.domain.ServiceEntry;
 
-import java.io.Serial;
+
 import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
 
 public class NamingServices {
 
